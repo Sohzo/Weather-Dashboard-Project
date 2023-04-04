8758a15c26fcf85f4f097548190b346658c46ee6
Jonathan Winter <jonpw2004@outlook.com>
2023-04-03 19:05:26 -0600

    Created forecast function

--- script.js
+++ script.js
@@ -5,10 +5,22 @@ var displaydate = document.getElementById("date");
 var displayday = document.getElementById("day");
 var displaypic = document.getElementById("weatherpic");
 var displayclouds = document.getElementById("clouds");
+var displayday1 = document.getElementById("day1");
+var displayday2 = document.getElementById("day2");
+var displayday3 = document.getElementById("day3");
+var displayday4 = document.getElementById("day4");
+var displayday5 = document.getElementById("day5");
+var displaydayname1 = document.getElementById("day1name");
+var displaydayname2 = document.getElementById("day2name");
+var displaydayname3 = document.getElementById("day3name");
+var displaydayname4 = document.getElementById("day4name");
+var displaydayname5 = document.getElementById("day5name");
+
 var search = document.getElementById("searchbar").value;
 var searchbutton = document.getElementById("searchbutton");
 var searchhistory = JSON.parse(localStorage.getItem("Search History") || "[]");
 var apiKey = "cd7053416bdb602f8d342304d55447b9"
+
 
 
 searchbutton.addEventListener('click', function() {
@@ -75,17 +87,27 @@ function getweather(search) {
             console.log(data)
             var lat = data[0].lat
             var lon = data[0].lon
-            var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey
+            var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
             fetch(forecastURL)
                 .then(function (response) {
                     return response.json()
                 })
                 .then(function (data) {
                     console.log(data)
+                    
+                    displayday1.innerHTML = data.list[3].main.temp + " &#176F";
+                    displayday2.innerHTML = data.list[11].main.temp + " &#176F";
+                    displayday3.innerHTML = data.list[19].main.temp + " &#176F";
+                    displayday4.innerHTML = data.list[27].main.temp + " &#176F";
+                    displayday5.innerHTML = data.list[35].main.temp + " &#176F";
+                    
+                    displaydayname1.innerHTML = ((new Date(data.list[3].dt*1000)).getMonth() + 1) + "/" + ((new Date(data.list[3].dt*1000)).getDate());
+                    displaydayname2.innerHTML = ((new Date(data.list[11].dt*1000)).getMonth() + 1) + "/" + ((new Date(data.list[11].dt*1000)).getDate());
+                    displaydayname3.innerHTML = ((new Date(data.list[19].dt*1000)).getMonth() + 1) + "/" + ((new Date(data.list[19].dt*1000)).getDate());
+                    displaydayname4.innerHTML = ((new Date(data.list[27].dt*1000)).getMonth() + 1) + "/" + ((new Date(data.list[27].dt*1000)).getDate());
+                    displaydayname5.innerHTML = ((new Date(data.list[35].dt*1000)).getMonth() + 1) + "/" + ((new Date(data.list[35].dt*1000)).getDate());
                 });
         });
     
-        
-        
 }
 
